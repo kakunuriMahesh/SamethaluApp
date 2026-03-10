9af560879f95441b35df2ef92999ad26507f7001
Mahesh Kakunuri <maheshkakunuri9@gmail.com>
2026-03-10 17:55:17 +0530

    updated api

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000';
+// const API_BASE = 'http://localhost:5000';
+const API_BASE = 'https://samethaluapp.onrender.com';
 
 const api = axios.create({ baseURL: API_BASE });
 
--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // Change this to your machine's LAN IP when testing on a physical device
 // e.g., 'http://192.168.1.100:5000'
-const BASE_URL = 'http://192.168.0.21:5000';
+// const BASE_URL = 'http://192.168.0.21:5000';
+const BASE_URL = 'https://samethaluapp.onrender.com';
 
 const api = axios.create({ baseURL: BASE_URL, timeout: 10000 });
 
